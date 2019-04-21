3b51450afcc38abcf45d647f845c760cfb5ffc8d
bordyboy <bogdanlivadaru@yahoo.com>
2019-04-21 11:34:37 -0700

    Work Submission

--- js/lab1.js
+++ js/lab1.js
@@ -5,7 +5,8 @@ function reverseString(str) {
 }
 
 function listSkills(){
-    let skills = ['Web Development','Gardening','Drumming','Volunteering';
+    // added closing bracket
+    let skills = ['Web Development','Gardening','Drumming','Volunteering'];
     let output = "<h2>Bruce's Skills<h2>";
     output += '<ul>';
     for (let i = 0; i < skills.length; i++) {
@@ -16,14 +17,18 @@ function listSkills(){
 }
 
 function getStarted(){
-    let firstName  'Bruce'
+    // no equal sign after "firstName"
+    // no semicolin at end of line 
+    let firstName = 'Bruce';
     let lastName = 'Elgort';
-    let collegeName = 'Clark College'
+    // no semicolin at end of line
+    let collegeName = 'Clark College';
     let city = 'Vancouver, Washington';
     let gpa = 3.2;
-    let output = `${firstName} {lastName} is currently attending ${collegeName}, located in {city} and has a grade point average of {gpa}`;
-
-    document.getElementById('place1').innerHTML = '<p>' + output + '</p>";
+    // no dollar sign before "lastName", "city" and "gpa"
+    let output = `${firstName} ${lastName} is currently attending ${collegeName}, located in ${city} and has a grade point average of ${gpa}`;
+    // single quote ending with double quote doesnt work, has to be same type
+    document.getElementById('place1').innerHTML = '<p>' + output + "</p>";
     document.getElementById('place2').innerHTML = '<p>' + reverseString(output) + '</p>';
 }
 
